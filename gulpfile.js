c1669fabdb269cc6819896e0f55dcc8fe56f5f2c
chichiwang <chichi.wang@ignitioncreative.com>
2014-09-17 12:51:51 -0700

    Reference Bundle Builder

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 var gulp = require('gulp');
+var uRequire = require('./node_modules/urequire/build/code/urequire.js')
 
 var noop = function(){
 	console.log('No Op', arguments);
 };
 
 gulp.task('default', function(){
-	console.log('default', arguments);
+	console.log('default', uRequire.BundleBuilder);
 	noop();
 });
